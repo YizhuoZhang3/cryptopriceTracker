de64911d3505d2055fa9f6fd09cfb9301d4a280e
Zoe Zhang <zhangyizhuozoe@gmail.com>
2022-02-05 22:02:43 -0500

    login page

--- Components/LoginScreen.js
+++ Components/LoginScreen.js
@@ -1,17 +1,73 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import React, { useState } from 'react';
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
-function LoginScreen(props) {
+function LoginScreen({navigation}) {
+  const [userName, setUserName] = useState('')
+  const handlePress = () => {
+    if (!userName.trim()) {
+      alert('Please Enter an User Name');
+      return;
+    }
+    navigation.navigate('Home')
+  }
   return (
-    <SafeAreaProvider style={styles.container}>
-      <View></View>
-    </SafeAreaProvider>
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <View style={styles.inputContainer}>
+      <TextInput
+          placeholder="username"
+          value={userName}
+          onChangeText={text => setUserName(text)}
+          style={styles.input}
+          required
+        />
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={handlePress}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputContainer: {
+    width: '60%'
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    width: '60%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: '#0782F9',
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
 });
 
 export default LoginScreen;
