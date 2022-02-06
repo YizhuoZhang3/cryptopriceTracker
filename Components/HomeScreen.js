ccbfe1bdbfe47c60c46df70307cfc5c6bf604f26
Zoe Zhang <zhangyizhuozoe@gmail.com>
2022-02-05 19:12:21 -0500

    first commit

---
+++ Components/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+function HomeScreen(props) {
+  return (
+    <SafeAreaProvider style={styles.container}>
+      <View></View>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {}
+});
+
+export default HomeScreen;
---
+++ Components/SplashScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+function SplashScreen(props) {
+  return (
+    <View style={styles.container}></View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {}
+});
+
+export default SplashScreen;
